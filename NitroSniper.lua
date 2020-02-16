7f6e9ad81920a86b22e8b90c67f0b14e76beb4a3
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-02-16 07:34:31 -0500

    zell you're a skid and will probably copy this src

---
+++ NitroSniper.lua
@@ -0,0 +1,38 @@
+local Discordia = require'discordia'
+local Client = Discordia.Client()
+local Http = require'coro-http'
+local sfind,slower,ssub = string.find,string.lower,string.sub
+local request = Http.request 
+
+local Token = args[2] or "" -- Token goes in the string or as a 2nd argument 
+if not Token or Token == "" then error'You need to put your token as a second argument or put it in the string.' end
+
+local function testnitro(Code)
+    local Head,Body = request("GET","https://ptb.discordapp.com/api/v6/entitlements/gift-codes/"..Code,{{"Authorization",Token}})
+    return Head.code == 200 and true
+end
+
+local function redeemcode(Code)
+    local Head,Body = request("POST","https://ptb.discordapp.com/api/v6/entitlements/gift-codes/"..Code.."/redeem",{{"content-type","application/json"},{"Authorization",Token}},'{"payment_source_id":null}')
+    return Head.code
+end
+
+Client:on('messageCreate',function(Message)
+local Content = Message.content
+local Find = sfind(slower(Content),"discord.gift/")
+    if Find then
+    local Gift = ssub(Content,Find + 13)
+        if #Gift == 16 and testnitro(ssub(Gift,1,16)) then 
+            if redeemcode(ssub(Gift,1,24)) == 200 then  -- lol just a check 
+                print('Claimed '..ssub(Gift,1,24)..' from '..Message.author.tag)
+           end
+        elseif #Gift == 24 and testnitro(ssub(Gift,1,24)) then 
+           if redeemcode(ssub(Gift,1,24)) == 200 then  -- lol just a check 
+                print('Claimed '..ssub(Gift,1,24)..' from '..Message.author.tag)
+           end
+        end
+    end
+end)
+
+
+Client:run(Token)
